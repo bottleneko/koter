c9a1106d74b1fe9feee2d9118059c405ea4f0301
Boris Murashov <bottleneko@gmail.com>
2020-01-13 07:36:42 +0300

    Epmd class fields

--- src/main/epmd/epmd.kt
+++ src/main/epmd/epmd.kt
@@ -1,5 +1,62 @@
 package epmd
 
+enum class EpmdProto(val opcode: Byte) {
+    ALIVE2_REQ(120),
+    ALIVE2_RESP(121),
+
+    PORT_PREASE2_REQ(122),
+    PORT2_RESP(119),
+
+    NAMES_REQ(110),
+
+    DUMP_REQ(100),
+    KILL_REQ(107),
+    STOP_REQ(115)
+}
+
+@UseExperimental(kotlin.ExperimentalUnsignedTypes::class)
+class Epmd(name: String, listenPort: UShort, epmdPort: UShort, hidden: Boolean = false) {
+    val fullName: String
+    val name: String
+    val domain: String
+
+    val port: UShort
+
+    val portEpmd: UShort
+
+    // http://erlang.org/doc/reference_manual/distributed.html (section 13.5)
+    val type: UByte
+
+    val protocol: UByte
+    val highVsn: UShort
+    val lowVsn: UShort
+    val extra: Array<Byte>
+    val creation: UShort
+
+    init {
+        val ns = name.split("@")
+	      if (ns.size != 2) {
+		        throw Exception("FQDN for node name is required (example: node@hostname)")
+	      }
+
+        fullName = name
+        this.name = ns[0]
+        domain = ns[1]
+
+        port = listenPort
+
+        portEpmd = epmdPort
+
+        type = if(hidden) 72u else 77u
+
+        protocol = 0u
+        highVsn = 5u
+        lowVsn = 5u
+        extra = arrayOf()
+        creation = 0u
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello world")
+    println("Hello world ${EpmdProto.ALIVE2_REQ.opcode}")
 }
